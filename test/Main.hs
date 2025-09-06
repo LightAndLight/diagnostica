379f7ee86ad2fc4098beb452eba7bcb2862f020c
Isaac Elliott <isaace71295@gmail.com>
2025-09-06 11:59:51 +1000

    Add failing test

--- test/Main.hs
+++ test/Main.hs
@@ -4,6 +4,8 @@ module Main where
 import qualified Data.Text.Lazy as Lazy
 import Text.Diagnostic
 import Test.Hspec
+import qualified Data.Text.Encoding as Text.Encoding
+import qualified Data.ByteString as ByteString
 
 main :: IO ()
 main =
@@ -77,6 +79,24 @@ main =
             , "\ESC[34;1m  |\ESC[39;0m"
             , "\ESC[34;1m1 | \ESC[39;0mthis is the second line"
             , "\ESC[34;1m  | \ESC[39;0m            \ESC[91;1m^\ESC[39;0m"
+            ]
+        input `shouldBe` output
+      it "non-ascii" $ do
+        let
+          line1 = "😎😎😎😎😎😎"
+          line2 = "error is here"
+          input =
+            render (defaultConfig { zeroIndexed = True }) "filename" (line1 <> "\n" <> line2) $
+            emit
+              (Offset $ ByteString.length (Text.Encoding.encodeUtf8 line1) + ByteString.length "error is " + 1)
+              (Span 4)
+              "this is a message"
+          output =
+            Lazy.unlines
+            [ "filename:0:0: \ESC[91;1merror: \ESC[39;0mthis is a message"
+            , "\ESC[34;1m  |\ESC[39;0m"
+            , "\ESC[34;1m0 | \ESC[39;0merror is here"
+            , "\ESC[34;1m  |          \ESC[39;0m\ESC[91;1m^^^^\ESC[39;0m"
             ]
         input `shouldBe` output
     describe "examples - 1-indexed" $ do
